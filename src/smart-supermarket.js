f4a8dd6683e95dc128d77a2cdd7f311ea190276c
Tommaso Bossi <tommasobossi95+github@gmail.com>
2018-04-28 13:12:59 +0200

    Refactoring server

---
+++ src/smart-supermarket.js
@@ -0,0 +1,67 @@
+'use strict';
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const sassMiddleware = require('node-sass-middleware');
+const createError = require('http-errors');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const publicDir = path.join(__dirname, 'public');
+const viewDir = path.join(__dirname, 'views');
+
+class smartSupermarket {
+    get expressApp() {
+        return this.app;
+    }
+
+    constructor(port) {
+        this.app = express();
+        this.app.set('port', port);
+        this.setViewEngine();
+        this.setOthers();
+        this.setRoutes();
+    }
+
+    setViewEngine() {
+        this.app.set('views', viewDir);
+        this.app.set('view engine', 'pug');
+    }
+
+    setOthers() {
+        this.app.use(logger('dev'));
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: false }));
+        this.app.use(cookieParser());
+        this.app.use(sassMiddleware({
+            src: publicDir,
+            dest: publicDir,
+            indentedSyntax: false, // true = .sass and false = .scss
+            sourceMap: true
+        }));
+    }
+
+    setRoutes() {
+        this.app.use(express.static(publicDir));
+
+        this.app.use('/', indexRouter);
+        this.app.use('/users', usersRouter);
+
+        this.app.use(function(req, res, next) {
+            next(createError(404));
+        });
+
+        this.app.use(function(err, req, res, next) {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+            // render the error page
+            res.status(err.status || 500);
+            res.render('error');
+        });
+    }
+}
+
+module.exports = smartSupermarket;
