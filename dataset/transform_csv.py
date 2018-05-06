d5bea77d5f844dac7b36ca05c59e28f623dbf636
Tommaso Bossi <tommasobossi95+github@gmail.com>
2018-05-06 16:34:20 +0200

    Changed dataset generator to add random availability and discount

--- dataset/transform_csv.py
+++ dataset/transform_csv.py
@@ -1,12 +1,9 @@
 import pandas as pd
 import re
+import random
 
-column_name = ['web-scraper-order', 'web-scraper-start-url',
-               'reparto', 'reparto-href', 'title', 'price']
-df = pd.read_csv(
-    'ilgigantebig.csv',
-    names=column_name
-)
+column_name = ['web-scraper-order', 'web-scraper-start-url', 'reparto', 'reparto-href', 'title', 'price']
+df = pd.read_csv('ilgigantebig.csv', names = column_name)
 print('\n--originale--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
@@ -16,21 +13,20 @@ print('\n--senza intestazione--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
-df = df.drop(
-    ['web-scraper-order', 'web-scraper-start-url', 'reparto-href'], axis=1)
+df = df.drop(['web-scraper-order', 'web-scraper-start-url', 'reparto-href'], axis = 1)
 print('\n--senza colonne url--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
-# Sistemo i prezzi con solo numeri float in point
+# Sistemo i prezzi con solo numeri floating point
 correct_price = []
 for index, row in df.iterrows():
-    price_str = row['price']
-    num = re.findall("\d+\,\d+", str(price_str))
-    if (len(num) != 0):
-        correct_price.append(float(re.sub(',', '.', num[0])))
-    else:
-        correct_price.append('')
+	price_str = row['price']
+	num = re.findall("\d+\,\d+", str(price_str))
+	if (len(num) != 0):
+		correct_price.append(float(re.sub(',', '.', num[0])))
+	else:
+		correct_price.append('')
 # print(correct_price)    
 df['price'] = correct_price
 print('\n--con prezzo numerico--')
@@ -43,12 +39,44 @@ print('\n--elimino colonne con prezzo non valido--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
+discount_price = []
+for index, row in df.iterrows():
+	price = row['price']
+	if (random.random() > 0.5):
+		discount_price.append(price)
+	else:
+		discount_price.append(float("%.2f" % (price * random.uniform(0.45, 1))))
+
+df['discount'] = discount_price
+print('\n--aggiungo sconto--')
+print('line length', len(df.index))
+print('column length', len(df.columns))
+
+# Aggiungo una quanità random:
+availability = []
+for index, row in df.iterrows():
+	availability.append(random.randint(2, random.randint(1, 6) * 5))
+
+df['availability'] = availability
+print('\n--aggiungo quantità--')
+print('line length', len(df.index))
+print('column length', len(df.columns))
+
 # Aggiungo id
 df = df.reset_index()
 print('\n--aggiungo index--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
-json_file = df.to_json(orient='records')
-with open("smart_supermarket-dataset.json", "w") as text_file:
-    text_file.write(json_file)
+df.rename(columns = {'reparto' : 'department', 'price' : 'original'}, inplace = True)
+json_file = (df.groupby(['index', 'department', 'title', 'availability'], as_index = False)
+			  .apply(lambda x: {
+			  		'original' : float(x.original),
+			  		'discount' : float(x.discount),
+			  		'currency' : "€"
+			  		})
+			  .reset_index()
+			  .rename(columns = {0 : 'price'})
+			  .to_json(orient = 'records', force_ascii = False))
+with open("smart_supermarket-dataset.json", "w", encoding = 'utf-8') as text_file:
+	text_file.write(json_file)
