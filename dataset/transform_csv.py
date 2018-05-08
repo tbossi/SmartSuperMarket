9dcc019e6b2f5fe5b01e780b00a97ea7fa67878a
Tommaso Bossi <tommasobossi95+github@gmail.com>
2018-05-08 17:38:48 +0200

    Added random shelf in dataset generation

--- dataset/transform_csv.py
+++ dataset/transform_csv.py
@@ -52,7 +52,7 @@ print('\n--aggiungo sconto--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
-# Aggiungo una quanità random:
+# Aggiungo una quantità random:
 availability = []
 for index, row in df.iterrows():
 	availability.append(random.randint(2, random.randint(1, 6) * 5))
@@ -62,14 +62,24 @@ print('\n--aggiungo quantità--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
+# Aggiungo uno scaffale random:
+shelf = []
+for index, row in df.iterrows():
+	shelf.append('S' + str(random.randint(1, 5)))
+
+df['shelf'] = shelf
+print('\n--aggiungo scaffale--')
+print('line length', len(df.index))
+print('column length', len(df.columns))
+
 # Aggiungo id
 df = df.reset_index()
 print('\n--aggiungo index--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
-df.rename(columns = {'reparto' : 'department', 'price' : 'original'}, inplace = True)
-json_file = (df.groupby(['index', 'department', 'title', 'availability'], as_index = False)
+df.rename(columns = {'reparto' : 'department', 'price' : 'original', 'availability' : 'value'}, inplace = True)
+json_file = (df.groupby(['index', 'department', 'title', 'shelf', 'value'], as_index = False)
 			  .apply(lambda x: {
 			  		'original' : float(x.original),
 			  		'discount' : float(x.discount),
@@ -77,6 +87,11 @@ json_file = (df.groupby(['index', 'department', 'title', 'availability'], as_ind
 			  		})
 			  .reset_index()
 			  .rename(columns = {0 : 'price'})
+			  .groupby(['index', 'department', 'title'], as_index = False)
+			  .apply(lambda x: x[['shelf','value']].to_dict('r'))
+			  .reset_index()
+			  .rename(columns = {0 : 'availability'})
 			  .to_json(orient = 'records', force_ascii = False))
+
 with open("smart_supermarket-dataset.json", "w", encoding = 'utf-8') as text_file:
 	text_file.write(json_file)
