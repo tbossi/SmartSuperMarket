8ba081bc861c65c9cb800372367b2cc50bf5e456
nicolalandro <nicolaxx94@live.it>
2018-05-17 16:23:28 +0200

    riparato db

--- dataset/transform_csv.py
+++ dataset/transform_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import random
 
-column_name = ['web-scraper-order', 'web-scraper-start-url', 'department', 'reparto-href', 'title', 'original_price']
+column_name = ['web-scraper-order', 'web-scraper-start-url', 'department', 'reparto-href', 'title', 'price']
 df = pd.read_csv('ilgigantebig.csv', names=column_name)
 print('\n--originale--')
 print('line length', len(df.index))
@@ -21,27 +21,27 @@ print('column length', len(df.columns))
 # Sistemo i prezzi con solo numeri floating point
 correct_price = []
 for index, row in df.iterrows():
-    price_str = row['original_price']
+    price_str = row['price']
     num = re.findall("\d+\,\d+", str(price_str))
     if (len(num) != 0):
         correct_price.append(float(re.sub(',', '.', num[0])))
     else:
         correct_price.append('')
 # print(correct_price)    
-df['original_price'] = correct_price
+df['price'] = correct_price
 print('\n--con prezzo numerico--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
 # Elimino tutti quelli che hanno prezzo vuoto (c'erano dei price nil)
-df = df[df['original_price'] != '']
+df = df[df['price'] != '']
 print('\n--elimino colonne con prezzo non valido--')
 print('line length', len(df.index))
 print('column length', len(df.columns))
 
 new_price = []
 for index, row in df.iterrows():
-    price = row['original_price']
+    price = row['price']
     discount = price
     if (random.random() > 0.5):
         discount = float("%.2f" % (price * random.uniform(0.45, 1)))
@@ -57,7 +57,7 @@ availability = []
 for index, row in df.iterrows():
     value = random.randint(2, random.randint(1, 6) * 5)
     shelf = 'S' + str(random.randint(1, 5))
-    availability.append({'value': value, 'shelf': shelf})
+    availability.append([{'value': value, 'shelf': shelf}])
 
 df['availability'] = availability
 print('\n--aggiungo quantità--')
