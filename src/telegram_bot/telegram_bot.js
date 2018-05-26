16cbd9ddb6a575ab1fa5e9ce17b3478ee6ecaa6c
nicolalandro <nicolaxx94@live.it>
2018-05-26 10:49:31 +0200

    telegram bot base version

---
+++ src/telegram_bot/telegram_bot.js
@@ -0,0 +1,37 @@
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+
+
+var token = fs.readFileSync('./token').toString();
+
+const bot = new TelegramBot(token, {polling: true});
+var arrayOfChat = [];
+
+bot.onText(/^(\/subscribe$)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    if (arrayOfChat.includes(chatId)) {
+        bot.sendMessage(chatId, 'Your subscription was accepted yet');
+    }
+    else {
+        arrayOfChat.push(chatId);
+        console.log(arrayOfChat.length);
+        bot.sendMessage(chatId, 'Your subscription is accepted');
+    }
+});
+
+bot.onText(/^(\/unsubscribe$)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    if (arrayOfChat.includes(chatId)) {
+        arrayOfChat.splice(arrayOfChat.indexOf(chatId), 1);
+        console.log(arrayOfChat.length);
+        bot.sendMessage(chatId, 'Your subscription is removed');
+    }
+    else {
+        bot.sendMessage(chatId, 'Your subscription was removed yet');
+    }
+});
+
+// bot.on('message', (msg) => {
+//     const chatId = msg.chat.id;
+//     bot.sendMessage(chatId, 'Received your message');
+// });
